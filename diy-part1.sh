a4e74b53655a69f76f6e4e60631a82b180150aaf
Kevin Li <ibinlike@gmail.com>
2022-08-08 15:00:21 +0800

    Update diy-part1.sh
    update passwall

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,6 @@
 
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
